d2ea736d3b01b252668b7366a21ac03d8f32d7da
마누 <manu.k@kakaocorp.com>
2022-05-09 12:05:30 +0900

    config(commitizen): conventional commit을 위한 커밋메시지 cli 추가

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,13 @@
 import type { UserConfig } from '@commitlint/types'
+import commitizenConfig from './.cz-config.js'
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', commitizenConfig.types.map(({ value }) => value)],
+    'type-case': [2, 'always', 'lower-case'],
+    'scope-empty': [2, 'never'],
+  },
 }
 
 module.exports = Configuration
